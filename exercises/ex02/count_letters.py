1343a891ff4cb376e15e2bd9e92f382996eff528
Eric Margolis <ejm@ad.unc.edu>
2021-09-09 01:33:08 -0400

    Through ex02

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -4,3 +4,14 @@ __author__ = "730329515"
 
 
 # Begin your solution here...
+lt: str = input("What letter do you want to search for? ")
+wd: str = input("Enter a word ")
+counter: int = 0
+index: int = 0
+letter: str = " "
+while index < len(wd):
+    letter = wd[index]
+    index = index + 1
+    if lt == letter:
+        counter = counter + 1
+print(counter)
